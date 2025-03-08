c8b867778e2274491f057a35d8e8c06ca2060dfd
jkuya <kuyajesee@proton.me>
2025-03-08 18:05:32 +0300

    feat: enhance ReadFile function to handle UTF-8 BOM and validate UTF-8 encoding

--- utils/readFile.go
+++ utils/readFile.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"bytes"
 	"errors"
 	"os"
+	"unicode/utf8"
 )
 
 func ReadFile(path string) ([]byte, error) {
@@ -10,5 +12,13 @@ func ReadFile(path string) ([]byte, error) {
 	if err != nil {
 		return nil, errors.New("error reading content of the file")
 	}
+
+	utf8BOM := []byte{0xEF, 0xBB, 0xBF}
+	file = bytes.TrimPrefix(file, utf8BOM)
+
+	if !utf8.Valid(file) {
+		return nil, errors.New("file contains invalid UTF-8")
+	}
+
 	return file, nil
 }
