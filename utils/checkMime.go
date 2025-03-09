9387a6a0f5e730b0d40e54f93307340dc120571a
jkuya <kuyajesee@proton.me>
2025-03-09 12:17:46 +0300

    feat(utils): add support for application/pdf MIME type in CheckMime

--- utils/checkMime.go
+++ utils/checkMime.go
@@ -6,6 +6,7 @@ func CheckMime(file []byte) bool {
 	allowedMIMEs := map[string]struct{}{
 		"text/plain":                {},
 		"text/plain; charset=utf-8": {},
+		"application/pdf":           {},
 	}
 
 	mimeType := http.DetectContentType(file)
